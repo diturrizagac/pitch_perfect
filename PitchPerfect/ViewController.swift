a27f56a21c5916d9922a8d060f23c64603bee3a5
Diego Raul Iturrizaga Casas <DiegoRaul.IturrizagaCasas@fisglobal.com>
2019-12-23 16:10:14 -0500

    first commit

---
+++ PitchPerfect/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PitchPerfect
+//
+//  Created by Diego Raúl Iturrizaga Casas on 12/19/19.
+//  Copyright © 2019 Diego Raúl Iturrizaga Casas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
